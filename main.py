c5f89264092c1c40e1f4267eb17d4372ac39018b
Najib Fikri <najibfikri13@gmail.com>
2023-12-01 22:38:02 +0700

    Apply embedding pattern to solve previous problem
    embedding pattern:
    - The first 64 bits are the decimal of the length of messages to be embedded
    - The next 8 bits are the decimal of the scalar value
    - and then the next bits is the message to be embedded (the message that has been modulated)

--- main.py
+++ main.py
@@ -1,61 +1,64 @@
 import cv2
-import numpy as np
 from pyspectra.core import core
 from pyspectra.analysis import analysis
 
 def main():
-    mode = input("embed/extract/analyze? ")
-    match mode:
-        case "embed":
-            # image_name = input("Masukkan nama gambar: ")
-            # key = input("Masukkan kunci: ")
-            # scalar = int(input("Masukkan nilai scalar: "))
-            # message = input("Masukkan pesan: ")
-            image_name = "lenna.bmp"
-            key = "lana"
-            scalar = 4
-            message = "coba"
+    while True:
+        mode = input("embed/extract/analyze? ")
+        if mode == "embed":
+            image_name = input("Masukkan nama gambar: ")
+            key = input("Masukkan kunci: ")
+            scalar = int(input("Masukkan nilai scalar: "))
+            message = input("Masukkan pesan: ")
+            output_name = input("Masukkan nama gambar output: ")
 
             pyspectra = core.PySpectra()
-            lenna = cv2.imread("./assets/" + image_name, 0)
+            try:
+                cover = cv2.imread("./assets/" + image_name, 0)
+                new_image = pyspectra.embedding(image=cover, message=message, key=key, scalar=scalar)
+                cv2.imwrite("./outputs/" + output_name, new_image)
+            except Exception as e:
+                print("Error:", e)
+                continue
 
-            new_image = pyspectra.embedding(image=lenna, message=message, key=key, scalar=scalar)
-
-            cv2.imwrite("./outputs/stego_image.bmp", new_image)
-
-        case "extract":
-            # stego_name = input("Masukkan nama stego: ")
-            # key = input("Masukkan kunci: ")
-            # scalar = int(input("Masukkan scalar: "))
-            stego_name = "stego_image.bmp"
-            key = "lana"
-            scalar = 4
+        elif mode == "extract":
+            stego_name = input("Masukkan nama stego: ")
+            key = input("Masukkan kunci: ")
 
             pyspectra = core.PySpectra()
-            stego = cv2.imread("./outputs/" + stego_name, 0)
+            try:
+                stego = cv2.imread("./outputs/" + stego_name, 0)
+                extracted_message = pyspectra.extract(image=stego, key=key)
+                print("pesan:", extracted_message)
+            except Exception as e:
+                print("Error:", e)
+                continue
 
-            extracted_message = pyspectra.extract(image=stego, key=key, scalar=scalar)
+        elif mode == "analyze":
+            try:
+                image1_path = "./assets/" + input("Masukkan path image 1: ")
+                image2_path = "./outputs/" + input("Masukkan path image 2: ")
 
-            print("pesan:", extracted_message)
+                image1 = cv2.imread(image1_path, 0)
+                image2 = cv2.imread(image2_path, 0)
 
-        case "analyze":
-            image1_path = "./assets/lenna.bmp"
-            image2_path = "./outputs/stego_image.bmp"
+                analyze = analysis.Analysis()
+                
+                psnr = analyze.psnrAnalysis(image1, image2)
+                ssim = analyze.ssimAnalysis(image1, image2)
 
-            image1 = cv2.imread(image1_path, 0)
-            image2 = cv2.imread(image2_path, 0)
+                # output
+                # PSNR: 84.91257479169302
+                # SSIM: 0.9999983332798968
 
-            analyze = analysis.Analysis()
-            
-            psnr = analyze.psnrAnalysis(image1, image2)
-            ssim = analyze.ssimAnalysis(image1, image2)
-
-            print("PSNR:", psnr)
-            print("SSIM:", ssim)
-
-            # output
-            # PSNR: 84.91257479169302
-            # SSIM: 0.9999983332798968
+                print("PSNR:", psnr)
+                print("SSIM:", ssim)
+            except Exception as e:
+                print("Error:", e)
+                continue
+        else:
+            print("Invalid mode. Please enter 'embed', 'extract', or 'analyze'.")
+            continue
 
 if __name__ == "__main__":
     main()
--- pyspectra/core/core.py
+++ pyspectra/core/core.py
@@ -4,13 +4,13 @@ class PySpectra(object):
     def __init__(self):
         pass
 
-    def _lsbEmbed(self, image=np.array([]), modulated_message="") -> np.array([]):
+    def _lsbEmbed(self, image=np.array([]), message="") -> np.array([]):
         if len(image.shape) != 2:  # Check if the image is grayscale
             raise ValueError("Input image should be a grayscale image")
 
         # Check if the message can fit within the image
         max_message_size = image.size 
-        if len(modulated_message) > max_message_size:
+        if len(message) > max_message_size:
             raise ValueError("Message size exceeds the capacity of the image")
 
         # Flatten the image to process pixel values
@@ -18,7 +18,7 @@ class PySpectra(object):
 
         # Embedding message in the least significant bit of each pixel
         bit_index = 0
-        for char in modulated_message:
+        for char in message:
             flat_image[bit_index] = (flat_image[bit_index] & 0xFE) | int(char)
             bit_index += 1
 
@@ -26,12 +26,14 @@ class PySpectra(object):
         stego_image = flat_image.reshape(image.shape)
         return stego_image
 
+
     def _getLSB(self, image=np.array([])) -> np.array([]):
         flat_image = image.flatten()
         lsbMessage = ""
         for i in range(len(flat_image)):
             lsbMessage += str(flat_image[i] & 1) # shortcut to get last bit
         return lsbMessage
+
 
     def _spreading(self, message="", scalar=2) -> str:
         message_bin = "".join(format(ord(i), '08b') for i in message)
@@ -41,15 +43,22 @@ class PySpectra(object):
             spreaded_message += i*scalar
             
         return spreaded_message
+    
 
     def _deSpreading(self, message="", scalar=2) -> str:
         original_message = ""
 
         for i in range(0, len(message), scalar):
-            original_message += message[i]
+            pair = message[i:i+scalar]
+
+            if all(bit == pair[0] for bit in pair):
+                original_message += pair[0]
+            else:
+                raise "Binary is broken, please check the key again"
             
         return original_message
     
+
     def _generateSeed(self, key="") -> str:
         key_binary = [format(ord(i), '08b') for i in key]
         bin_left = key_binary[0]
@@ -65,6 +74,7 @@ class PySpectra(object):
             bin_left = bin_result
             
         return int(bin_left, 2)
+
 
     def _generatePseudoNoise(self, seed=0, length=2) -> str:
         # formula LCG -> Xn+1 = (a . Xn + c) mod m
@@ -83,8 +93,8 @@ class PySpectra(object):
         
         return result        
 
+
     def _modulation(self, message="", pseudo_noise="") -> str:
-        pass
         # modulate message with pseudo noise using XOR operation
         # ensure the length is same
         if len(message) != len(pseudo_noise):
@@ -103,8 +113,8 @@ class PySpectra(object):
 
         return modulated_result
 
+
     def _spreadSpectrumEmbed(self, message="", key="", scalar=2) -> str:
-        pass
         # 1. spread message with scalar value
         # 2. generate pseudo noise with length of message that have been spreaded
         # 3. modulate that message with pseudo noise that have been generate
@@ -112,11 +122,12 @@ class PySpectra(object):
         spreaded_message= self._spreading(message=message, scalar=scalar)
 
         # generate pseudo number
+        seed = self._generateSeed(key)
         inc = 1
-        pseudo_number = self._generatePseudoNoise(self._generateSeed(key), inc*scalar)
+        pseudo_number = self._generatePseudoNoise(seed=seed, length=inc*scalar)
         while len(pseudo_number) < len(spreaded_message):
             inc += 1
-            pseudo_number = self._generatePseudoNoise(self._generateSeed(key), inc*scalar)
+            pseudo_number = self._generatePseudoNoise(seed=seed, length=inc*scalar)
 
         # modulate message
         modulated_message = self._modulation(spreaded_message, pseudo_number)
@@ -124,30 +135,19 @@ class PySpectra(object):
         return modulated_message
 
 
-
     def _spreadSpectrumExtract(self, modulated_message="", key="", scalar=2) -> str:
-        pass
         # 1. generate pseudo noise signal
         # 2. demoduate
         # 3. de spreading
         # 4. get the extracted message
 
-        pseudo_number = self._generatePseudoNoise(self._generateSeed(key), len(modulated_message)//8)
+        pseudo_number = self._generatePseudoNoise(seed=self._generateSeed(key), length=len(modulated_message)//8)
         spreaded_message = self._modulation(message=modulated_message, pseudo_noise=pseudo_number)
         original_message = self._deSpreading(message=spreaded_message, scalar=scalar)
 
         return original_message
-
-    def embedding(self, image=np.array([]), message="", key="", scalar=2) -> np.array([]):
-        pass
-        # 1. modulate message with Spread Spectrum Core
-        # 2. embed message in to the image LSB Core
-        modulated_message = self._spreadSpectrumEmbed(message=message, key=key, scalar=scalar)
-        print("[embedding] modulated message:", modulated_message)
-        embedded_image = self._lsbEmbed(image=image, modulated_message=modulated_message)
-
-        return embedded_image
     
+
     def _fromBinToText(self, bin_string="") -> str:
         binary_list = [bin_string[i:i+8] for i in range(0, len(bin_string), 8)]
 
@@ -155,14 +155,38 @@ class PySpectra(object):
         text = ''.join(chr(int(binary, 2)) for binary in binary_list)
 
         return text
+    
 
-    def extract(self, image=np.array([]), key="", scalar=2):
-        pass
+    def _getPattern(self, lsbData=""):
+        return lsbData[:64], lsbData[64:64+8], lsbData[64+8:]
+    
+
+    def embedding(self, image=np.array([]), message="", key="", scalar=2) -> np.array([]):
+        # 1. modulate message with Spread Spectrum Core
+        # 2. embed message in to the image LSB Core
+
+        # pattern -> first 64 char is length modulated message, 8 next char is scalar value, and next is the modulated message
+        modulated_message = self._spreadSpectrumEmbed(message=message, key=key, scalar=scalar)
+
+        length_modulated_message = len(modulated_message)
+        bin_len_modulated = bin(length_modulated_message)[2:].zfill(64)
+        bin_len_scalar = bin(scalar)[2:].zfill(8)
+
+        message = bin_len_modulated + bin_len_scalar + modulated_message
+
+        embedded_image = self._lsbEmbed(image=image, message=message)
+
+        return embedded_image
+
+
+    def extract(self, image=np.array([]), key=""):
         # 1. Get the last bit of an image
         # 2. extract message with spread spectrum extract
-        lsbMessage = self._getLSB(image=image)[:128]
-        original_message = self._spreadSpectrumExtract(modulated_message=lsbMessage, key=key, scalar=scalar)
+        lsbData = self._getLSB(image=image)
+        lenMessageBin, scalarBin, message = self._getPattern(lsbData=lsbData)
+        lenMessage = int(lenMessageBin, 2)
+        scalar = int(scalarBin, 2)
+
+        original_message = self._spreadSpectrumExtract(modulated_message=message[:lenMessage], key=key, scalar=scalar)
 
         return self._fromBinToText(original_message)
-    
-        
